9d58d9dd11937bad2f115b1807943585b157ab2c
adrianolinux <adrianolinux@gmail.com>
2013-12-08 17:43:41 -0200

    updating the technique

--- force_test.m
+++ force_test.m
@@ -1,22 +1,19 @@
+clear all;
 x = load('mammals.data');
-t = x(:,size(x,2));
-x = x(:,1:size(x,2)-1);
-n = size(x,1);
-
-iter = 20;
+t = x(:,end);
+x = x(:,1:end-1);
 
 tic
-[y,z] = force(x,iter);
+y = force(x);
 toc
 
 % plot results
 figure(1);
 clf;
 hold on;
-plot(y(t == 1,1), y(t == 1,2), 'r+', ...
-     y(t == 2,1), y(t == 2,2), 'g+', ...
-     y(t == 3,1), y(t == 3,2), 'b+', ...
-     y(t == 4,1), y(t == 4,2), 'k+');
+color = [0,0,0; 1,0,0; 0,1,0; 0,0,1];
+h = scatter(y(:,1), y(:,2), 25, color(t,:), 'filled');
+set(h,'markeredgecolor','k');
 hold off;
 
 %saveas(gcf,'figura.png');
